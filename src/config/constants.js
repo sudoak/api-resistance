76bbea956a742b1e4d7122e1c3d1469bb5043f61
sudoak <akram@coffeebeans.io>
2021-02-18 13:00:17 +0530

    feat: new ip

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,5 +2,6 @@
 module.exports.users = [
   { id: "api-admin", ip: "112.133.237.52" },
   { id: "api-admin", ip: "143.122.18.203" },
-  { id:"ak", ip: "143.122.18.244" } 
+  { id: "ak", ip: "143.122.18.244" },
+  { id: "sai", ip: "206.218.88.229" } 
 ];
